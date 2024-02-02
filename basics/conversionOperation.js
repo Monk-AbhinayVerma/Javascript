b43e9c4354424b34341fecc074a160621490855c
Monk-AbhinayVerma <53249427+Monk-AbhinayVerma@users.noreply.github.com>
2024-02-02 11:43:00 +0000

    conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+
+//null -> 0
+//"33abc" -> NaN
+//typedef "33abc" -> number
+//boolean(1)-> true
+//boolean("")-> false
+//boolean("hello")-> true
+
+//String(33) -> "33"
