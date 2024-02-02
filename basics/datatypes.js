cbe54eba1d3eb8a1dedb43a081378949b04cf1a2
Monk-AbhinayVerma <53249427+Monk-AbhinayVerma@users.noreply.github.com>
2024-02-02 11:25:49 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all js code as newer version
+ // alert is not used in nodejs but used in browser
+
+ let name = "Abhinay"
+ let age = 18
+ let isLoggedIn = false
+
+ //number => 
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined => no value assigned
+//symbol => unique
+
+//object
+
+console.log(typeof null)
