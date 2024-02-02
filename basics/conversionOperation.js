b51dfaa88940af261e8c2560d314a227ce7a840e
Monk-AbhinayVerma <53249427+Monk-AbhinayVerma@users.noreply.github.com>
2024-02-02 11:53:08 +0000

    Operators

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(valueInNumber)
+//console.log(valueInNumber)
 
 //null -> 0
 //"33abc" -> NaN
@@ -14,3 +14,33 @@ console.log(valueInNumber)
 //boolean("hello")-> true
 
 //String(33) -> "33"
+
+/****************Operations */
+
+let value = 3
+let negvalue = -value
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)power
+//console.log(2%2)remainder
+//console.log(2/2)
+
+let str1 = "hello"
+let str2 = "abhi"
+
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2);-12
+// console.log(1+"2");-12
+// console.log("1"+2+2);-122
+// console.log(1+2+"2");-32
+
+//console.log(+true) -1
+// console.log(+"") - 0
+
+// let count = 1;
+// ++count;
+// console.log(count) -2
