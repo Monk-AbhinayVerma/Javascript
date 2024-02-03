155e284c6c38515d1eab570580b0c481a326a54c
Monk-AbhinayVerma <53249427+Monk-AbhinayVerma@users.noreply.github.com>
2024-02-03 11:25:30 +0000

    date and time

---
+++ basics/dateAndtime.js
@@ -0,0 +1,7 @@
+let myDate = new Date()
+console.log(myDate) //2024-02-03T06:38:04.212Z
+
+console.log(myDate.toString()) //Sat Feb 03 2024 06:39:12 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString()) //Sat Feb 03 2024
+
+//see more - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
