dc3bdbc0a88447faa202aa4638c0a0a1fc4c95a3
Monk-AbhinayVerma <53249427+Monk-AbhinayVerma@users.noreply.github.com>
2024-02-03 06:24:27 +0000

    String and number

---
+++ basics/number_maths.js
@@ -0,0 +1,20 @@
+const score = 400 // number
+
+const balance = new Number(100) // nUMBER OBJECT
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')) - 10,00,000
+
+
+//--------Maths----------------
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(2.4644));
+
+console.log(Math.random()) // value between 0 & 1
+
+const min = 10
+const max= 20
+
+console.log(Math.floor(Math.random()*(max - min +1))+min) // value between min and max
---
+++ basics/string.js
@@ -0,0 +1,14 @@
+//Defining String in javascript
+//  const name = 'Abhinay'
+//  console.log(`Hi my name is ${name}`);
+
+ // Another way
+
+ const getCourse = new String('Masters')
+
+//  console.log(getCourse) - It returns object with various properties [String: 'Masters']
+
+// console.log(getCourse[0]) - M
+// console.log(getCourse.length) - 7
+
+//Read more - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
