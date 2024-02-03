d54d2c61aa6eeb53b562f3c109c18f431f25ac23
Monk-AbhinayVerma <53249427+Monk-AbhinayVerma@users.noreply.github.com>
2024-02-03 20:07:03 +0000

    scopes

---
+++ basics/scopes.js
@@ -0,0 +1,10 @@
+// if(true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+// console.log(a); - error - block scope
+// console.log(b); - error
+// console.log(c); - 30(global scope)
+
